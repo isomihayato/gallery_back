955118ad0ea6884b32bf1152f91575ea0965a8bd
ice-crypto <testtest2tom@gmail.com>
2021-06-08 15:01:30 +0900

    config/initialize/core.rb 本番用

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /localhost\:\d+/
-
+    origins /localhost\:\d+/, /gallery-react-front\.herokuapp\.com/
+    
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
